e50d338f3170f1df30424286f49846597bd42a63
komarbv211 <andrijkoslak@gmail.com>
2025-03-22 18:42:26 +0200

    added profile page

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,17 +4,23 @@ import { apiProduct } from '../services/apiProduct.ts';
 import {authApi} from "../services/authApi.ts";
 import userReducer from './slices/userSlice';
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { apiUser } from '../services/apiUser.ts';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         [apiCategory.reducerPath]: apiCategory.reducer,
         [apiProduct.reducerPath]: apiProduct.reducer,
-        [authApi.reducerPath]: authApi.reducer, // Додаємо API reducer
+        [authApi.reducerPath]: authApi.reducer,
+        [apiUser.reducerPath]: apiUser.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiCategory.middleware, apiProduct.middleware,
-            authApi.middleware), // Додаємо API middleware
+        getDefaultMiddleware().concat(
+            apiCategory.middleware, 
+            apiProduct.middleware,
+            authApi.middleware,
+            apiUser.middleware
+        ), 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
